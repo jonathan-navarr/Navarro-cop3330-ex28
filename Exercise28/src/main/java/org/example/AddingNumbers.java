100d28d7856cbb3e6535b100b4f7668d3d228e9c
jonathan-navarr <84359494+jonathan-navarr@users.noreply.github.com>
2021-06-13 21:42:07 -0400

    Add files via upload

---
+++ Exercise28/src/main/java/org/example/AddingNumbers.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Jonathan Navarro
+ */
+
+public class AddingNumbers {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        int i, number, sum = 0;
+        for(i = 0; i <= 4; i++ )
+        {
+            System.out.print("Enter a number: ");
+            number = scnr.nextInt();
+            sum = sum + number;
+
+        }
+        System.out.print("The sum of the number is: " + sum);
+
+    }
+
+}
